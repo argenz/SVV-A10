bb002bbdff597bd55981e73c32cc0e8e2af22d01
Joao Pereira <PereiraJoao@wlan-145-94-164-151.wlan.tudelft.nl>
2019-02-18 18:39:58 +0100

    'update idealization joao '

--- Idealization/Idealization.py
+++ Idealization/Idealization.py
@@ -19,9 +19,14 @@ B_coordinates = B_coordinatesTop + B_coordinatesRev[1:]#stiffner coordinates top
 
 A_st = 0.0012*(0.015-0.0012) + (0.02*0.0012) #stiffner area [m]
 
-#B1 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[16,0]/)+()) 
-
-
+B0 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[16,1]/B_coordinates[0,1])+(B_coordinates[16,1]/B_coordinates[1,1])) 
+B1 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[0,1]/B_coordinates[1,1])+(B_coordinates[2,1]/B_coordinates[1,1]))
+B2 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[1,1]/B_coordinates[2,1])+(B_coordinates[3,1]/B_coordinates[2,1]))
+B3 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[2,1]/B_coordinates[3,1])+(B_coordinates[4,1]/B_coordinates[3,1]))
+B4 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[3,1]/B_coordinates[4,1])+(B_coordinates[5,1]/B_coordinates[4,1]))
+#B5 = 
+#B6 = 
+B7 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[16,1]/B_coordinates[0,1])+(B_coordinates[16,1]/B_coordinates[1,1]))
 
 
 B_coordinates = np.array(B_coordinates)
