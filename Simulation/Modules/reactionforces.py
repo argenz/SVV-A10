d2e5184c52adc399b7e1e518f6dfde5c2151c8b0
Stijn van Selling <svanselling@student.tudelft.nl>
2019-02-18 18:29:43 +0100

    Fixed bugs

--- Simulation/Modules/reactionforces.py
+++ Simulation/Modules/reactionforces.py
@@ -10,18 +10,18 @@ import sympy
 exec(open("../Data.txt").read())
 I = 1E-10
 
-R = 0#(-q*la*(0.25*Ca-0.5*h)-P)/(np.cos(theta)*0.5*h)
+R = 1#(-q*la*(0.25*Ca-0.5*h)-P)/(np.cos(theta)*0.5*h)
 y_force = sympy.Matrix([[(1/6)*(x2-x1)**3, 0, 0, x2, 1, (1/24)*q*x2**4],
                           [0, 0, 0, x1, 1, d1*E*I + (1/24)*q*x1**4],
                           [(1/6)*(x3-x1)**3, (1/6)*(x3-x2)**3, 0, x3, 1, d3*E*I + (1/24)*q*x3**4],
                           [x2-x1, 0, -x3+x2, 0, 0, 0.5*q*x2**2 - 0.5*q*(x3-x2)**2],
                           [1, 1, 1, 0, 0, la*q]])
 
-z_force = sympy.Matrix([[1,1,1,0,0,-R-P],
-                        [x2-x1,0,x2-x3,0,0,-0.5*xa*(R-P)],
+z_force = sympy.Matrix([[-1,-1,-1,0,0,R+P],
+                        [x1-x2,0,x3-x2,0,0, -0.5*xa*(P-R)],
                         [0,0,0,x1,1,0],
-                        [-(1/6)*(x2-x1)**3,0,0,x2,1,(1/6)*(0.5*xa)**3*R],
-                        [-(1/6)*(x3-x1)**3,-(1/6)*(x3-x2)**3,0,x3,1,-(1/6)*(x3-(x2-0.5*xa))**3*R -(1/6)*(x3-(x2+0.5*xa))**3*P]])
+                        [(1/6)*(x2-x1)**3,0,0,x2,1,-(1/6)*(-0.5*xa)**3*R],
+                        [(1/6)*(x3-x1)**3, (1/6)*(x3-x2)**3,0,x3,1, -(1/6)*(x3-(x2-0.5*xa))**3*R - (1/6)*(x3-(x2+0.5*xa))**3*P]])
 
 
 rrefy = y_force.rref()[0]
@@ -33,7 +33,7 @@ z1, z2, z3, zA, zB = rrefz[5], rrefz[11], rrefz[17], rrefz[23], rrefz[29]
 def test_reactionforcesy():
     margin = 0.0001
     assert -margin <= y1 + y2 + y3 -la*q <= margin
-    assert -margin <= int((x2-x1)*y1 - (x3-x2)*y3 - 0.5*q*x2**2 + 0.5*q*(x3-x2)**2) <= margin
+    assert -margin <= (x2-x1)*y1 - (x3-x2)*y3 - 0.5*q*x2**2 + 0.5*q*(x3-x2)**2 <= margin
     assert -margin <= - d1 * E * I - (1/24)*q*x1**4 + yA*x1+ yB <= margin
     assert -margin <= - (1/24)*q*x2**4 + (1/6)*(x2-x1)**3*y1 + yA*x2 + yB <= margin
     assert -margin <= - d3*E*I - (1/24)*q*x3**4 + (1/6)*(x3-x1)**3*y1 + (1/6)*(x3-x2)**3*y2 + yA*x3 + yB <= margin
@@ -45,8 +45,8 @@ def test_reactionforcesz():
     assert -margin <= z1 + z2 + z3 + R + P <= margin
     assert -margin <= -(x3-x2)*z3+(x2-x1)*z1+0.5*xa*(R-P)<= margin
     assert -margin <= zA*x1+zB <= margin
-    assert -margin <= zA*x2+zB-(1/6)*(x2-x1)**3*z1-(1/6)*(0.5*xa)**3*R <= margin
-    assert -margin <= zA*x3+zB-(1/6)*(x3-x1)**3*z1-(1/6)*(x3-(x2-0.5*xa))**3*R-(1/6)*(x3-x2)**3*z2-(1/6)*(x3-(x2+0.5*xa))**3*P <= margin
+    assert -margin <= zA*x2+zB  + (1/6)*(x2-x1)**3*z1 + (1/6)*(-0.5*xa)**3*R <= margin
+    assert -margin <= zA*x3+zB + (1/6)*(x3-x1)**3*z1 + (1/6)*(x3-(x2-0.5*xa))**3*R + (1/6)*(x3-x2)**3*z2 + (1/6)*(x3-(x2+0.5*xa))**3*P <= margin
 
 
 test_reactionforcesz()
