9725f7ee4af0b1711a80322fc9cee973eb6cb131
Joao Pereira <PereiraJoao@wlan-145-94-164-151.wlan.tudelft.nl>
2019-02-18 18:26:01 +0100

    idealization for stijn to check out, not completed

---
+++ Idealization/Idealization.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 17:15:57 2019
+
+@author: PereiraJoao
+"""
+from IdealizationCoordinates import IdealizationCoordinates
+import numpy as np
+import matplotlib.pyplot as plt
+
+t_sk = 0.0011 #skin thikness
+t_sp = 0.0029 #spar thikness
+
+B_coordinatesTopPanel,B_coordinatesTopArc,b_st = IdealizationCoordinates() 
+B_coordinatesTop = B_coordinatesTopPanel + B_coordinatesTopArc 
+B_coordinatesRev = B_coordinatesTop[::-1]
+B_coordinates = B_coordinatesTop + B_coordinatesRev[1:]#stiffner coordinates top half of aileron
+
+A_st = 0.0012*(0.015-0.0012) + (0.02*0.0012) #stiffner area [m]
+
+#B1 = A_st + ((t_sk*b_st)/6)*(4+(B_coordinates[16,0]/)+()) 
+
+
+
+
+B_coordinates = np.array(B_coordinates)
+plt.plot(B_coordinates[:,0],B_coordinates[:,1])
+plt.show()
