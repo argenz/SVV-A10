fb6a9ecca9745570208367e2b357ee0be363cd85
Dirk <dirk_de_boer@hotmail.com>
2019-02-22 17:03:24 +0100

    updated

--- Simulation/Modules/findanglehinge2.py
+++ Simulation/Modules/findanglehinge2.py
@@ -12,24 +12,24 @@ ntotal=10000
 right=1
 left=-1
 initialthetay=0
-initialthetaz=0#np.deg2rad(-0.08783658335221914)
+initialthetaz=np.deg2rad(-0.08783658335221914)
 
 #deflectionlist1,xlist= deflectiondistributedload(q,la,ntotal,right,initialtheta)
 #deflectionlist2,xlist2= deflectionpointload(P,L_force,la,ntotal,right,initialtheta)
 
 #z 
 deflectionlistZ1,x=deflectionpointload(Z1,x2-x1,x2,ntotal,right, initialthetaz,Iyyrotated())
-deflectionlistZ2,x=deflectionpointload(Z2,0.0,x2,ntotal,right, initialthetaz,Iyyrotated()) #goes through start of wall, causes no deflection?
+deflectionlistZ2,x=deflectionpointload(Z2,0.0,x2,ntotal,right, 0,Iyyrotated()) 
 deflectionlistZ3,xnegativedirection=deflectionpointload(Z3,x3-x2,la-x2,ntotal,left, -initialthetaz,Iyyrotated())
-deflectionlistP,xnegativedirection=deflectionpointload(P,xa/2.,la-x2,ntotal,left, -initialthetaz,Iyyrotated())
-deflectionlistR,x= deflectionpointload(R,xa/2.,x2,ntotal,right, initialthetaz,Iyyrotated())
+deflectionlistP,xnegativedirection=deflectionpointload(P,xa/2.,la-x2,ntotal,left,0 ,Iyyrotated())
+deflectionlistR,x= deflectionpointload(R,xa/2.,x2,ntotal,right, 0,Iyyrotated())
 
 #y
 deflectionlistY1,x=deflectionpointload(Y1,x2-x1,x2,ntotal,right, initialthetay,Izzrotated())
-deflectionlistY2,x=deflectionpointload(Y2,0.0,x2,ntotal,right, initialthetay,Izzrotated()) #goes through start of wall, causes no deflection?
+deflectionlistY2,x=deflectionpointload(Y2,0.0,x2,ntotal,right, 0,Izzrotated()) 
 deflectionlistY3,xnegativedirection=deflectionpointload(Y3,x3-x2,la-x2,ntotal,left, -initialthetay,Izzrotated())
-deflectionlistq1,x=deflectiondistributedload(q,x2,ntotal,right, initialthetay,Izzrotated())
-deflectionlistq2,xnegativedirection=deflectiondistributedload(q,la-x2,ntotal,left, -initialthetay,Izzrotated())
+deflectionlistq1,x=deflectiondistributedload(q,x2,ntotal,right,0,Izzrotated())
+deflectionlistq2,xnegativedirection=deflectiondistributedload(q,la-x2,ntotal,left, 0,Izzrotated())
 
 dzright= np.array(deflectionlistZ1)+np.array(deflectionlistR)
 dzleft=np.array(deflectionlistP)+np.array(deflectionlistZ3)
