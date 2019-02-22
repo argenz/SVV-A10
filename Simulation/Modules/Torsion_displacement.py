2389ae89ffb58418b7c8ec2bd02b25c5189ed101
Stijn van Selling <svanselling@student.tudelft.nl>
2019-02-22 15:40:49 +0100

    Added comments to main, tools, TD, RF.

--- Simulation/Modules/Torsion_displacement.py
+++ Simulation/Modules/Torsion_displacement.py
@@ -4,6 +4,8 @@ Created on Wed Feb 20 09:10:45 2019
 
 @author: Stijn
 """
+
+# Reading data and initializing libraries.
 import numpy as np
 import matplotlib.pyplot as plt
 from Modules.reactionforces import *
@@ -12,66 +14,78 @@ from Modules.Tools import *
 from Modules.MOI import *
 from Modules.shear_center import *
 from sympy import *
-
 exec(open("./Data.txt").read())       
+
+# Calculation of reaction forces.
 X2,Y1,Y2,Y3,Z1,Z2,Z3,R = reaction_forces(1E-10)
 X2,Y1,Y2,Y3,Z1,Z2,Z3,R_y,R_z,P_y,P_z,Q_y,Q_z = transform(X2,Y1,Y2,Y3,Z1,Z2,Z3,R,P,q, theta)
 
+# Calculation of the upper aileron length.
 length_upper = 0.25 * np.pi * h + np.sqrt((h/2)**2 + (Ca-h/2)**2)
 
+# Calculate space between stringers.
 spacing = (length_upper - wst * 8.5)/9 
+
+# Initialize array of thicknesses wrt the position. 
+# Length of 35 because 3 + 8*4 = 35
+# Width of 3 because [Start, Stop, Thickness]
 thickness = np.zeros((35,3))
 
+# First 3 sections added. (Half of the stringer at the LE)
 thickness[0] = np.array([0,tst/2,hst + tsk])
 thickness[1] = np.array([thickness[0][1],thickness[0][1] + wst/2 - tst/2 ,tst + tsk])
 thickness[2] = np.array([thickness[1][1],thickness[1][1] + spacing ,tsk])
 
+# Adding the other sections.
 for i in range(3,len(thickness)):
-    if i%4 == 2:
-        thickness[i] = np.array([thickness[i-1][1],thickness[i-1][1] + spacing ,tsk])
-    if i%4 == 1:
-        thickness[i] = np.array([thickness[i-1][1],thickness[i-1][1] + wst/2 - tst/2 ,tst + tsk])
     if i%4 == 0:
         thickness[i] = np.array([thickness[i-1][1],thickness[i-1][1] + tst/2 ,hst + tsk])
+    if i%4 == 1:
+        thickness[i] = np.array([thickness[i-1][1],thickness[i-1][1] + wst/2 - tst/2 ,tst + tsk])
+    if i%4 == 2:
+        thickness[i] = np.array([thickness[i-1][1],thickness[i-1][1] + spacing ,tsk])
     if i%4 == 3:
         thickness[i] = np.array([thickness[i-1][1],thickness[i-1][1] + wst/2 ,tst + tsk])
-        
 
-
+# Obtaining the location of the shear center.
 shear_center = 0.03 #get_ShearCenter(get_Izz(),tsk,h,Ca)
-def torsion(x):
-    x += x2
 
-    T_aero = ((0.25*Ca - h/2) - shear_center) * Q_y * x
-    T_Y1   = Y1 * (shear_center)
-    T_Y2   = Y2 * (shear_center)
-    T_Y3   = Y3 * (shear_center)
-    T_P_y  = P_y * (h/2 - shear_center)
-    T_R_y  = R_y * (h/2 - shear_center)
-    T_P_z  = P_z * (h/2)
-    T_R_z  = R_z * (h/2)
+# Internal torsion as a function of X (x = 0 at hinge 2.)
+def torsion(x):
+    x += x2                                                 # Transfer to x = 0 at the root of the aileron.
+
+    T_aero = ((0.25*Ca - h/2) - shear_center) * Q_y * x     # Torque caused by the aerodynamic load q.
+    T_Y1   = Y1 * (shear_center)                            # Torque caused by Y1.
+    T_Y2   = Y2 * (shear_center)                            # Torque caused by Y2.
+    T_Y3   = Y3 * (shear_center)                            # Torque caused by Y3.
+    T_P_y  = P_y * (h/2 - shear_center)                     # Torque caused by P_y.
+    T_R_y  = R_y * (h/2 - shear_center)                     # Torque caused by R_y.
+    T_P_z  = P_z * (h/2)                                    # Torque caused by P_z.
+    T_R_z  = R_z * (h/2)                                    # Torque caused by R_z.
     
-    if 0 < x <= x1:
+    # Checking in what section x lies.
+    if 0 < x <= x1:                                                             # section 1
         return T_aero
-    if x1 < x <= -xa/2 + x2:
+    if x1 < x <= -xa/2 + x2:                                                    # section 2
         return T_aero + T_Y1
-    if -xa/2 + x2 < x <= x2:
+    if -xa/2 + x2 < x <= x2:                                                    # section 3
         return T_aero + T_Y1 + T_R_y + T_R_z
-    if x2 < x <= x2 + xa/2:
+    if x2 < x <= x2 + xa/2:                                                     # section 4
         return T_aero + T_Y1 + T_R_y + T_R_z + T_Y2
-    if x2 + xa/2 < x <= x3:
+    if x2 + xa/2 < x <= x3:                                                     # section 5
         return T_aero + T_Y1 + T_R_y + T_R_z + T_Y2 + T_P_y + T_P_z
-    if x3 < x <= la:
+    if x3 < x <= la:                                                            # section 6
         return T_aero + T_Y1 + T_R_y + T_R_z + T_Y2 + T_P_y + T_P_z + T_Y3
     return 0 
     
+# Area of the two cells, counted from LE to TE.
 Area_I  = 0.5 * np.pi*(h/2)**2
 Area_II = h*(Ca-h/2)
 
 
 
+# Splitting the thickness array into the curved part (a) and straight part (b).
 n = 0
-#split_thickness
 while thickness[n][1] < 2*np.pi*h/2*0.25:
     thickness_a = thickness[:n+2]
     thickness_b = thickness[n+2:]
@@ -80,7 +94,7 @@ thickness_b = np.row_stack((np.array([2*np.pi*h/2*0.25,thickness_b[0][0],thickne
 thickness_a[-1][1] = 2*np.pi*h/2*0.25
 
 
-
+# Calculating the line integrals
 sum_I_arc = 0
 sum_I_spar = 0
 for section in thickness_a:
@@ -94,6 +108,7 @@ for section in thickness_b:
     sum_II_tri += 2*(section[1]-section[0])/section[2]
 sum_II_spar += h/(tsp)
 
+# Solving the Torsion formula.
 angle = [0]
 x = [-x2]
 n = 1000
@@ -106,6 +121,7 @@ for xi in np.linspace(-x2,la-x2,n):
     x.append(xi)
     angle.append(angle[-1] + np.rad2deg(float(sol))*((-x2-la+x2)/n))
 
+# Plotting the angle over the length of the aileron.
 plt.plot(x,angle,color = 'r')
 plt.gca().invert_xaxis()
 
--- Simulation/Modules/reactionforces.py
+++ Simulation/Modules/reactionforces.py
@@ -3,45 +3,49 @@
 Created on Mon Feb 18 15:53:09 2019
 
 @author: Stijn
+
+This function calculated the forces on the aileron in the original(fixed to the wing) coordinate system
 """
+# Reading data and initializing libraries.
 import numpy as np
 import sympy
-
-
 exec(open("./Data.txt").read())
 
-#R = (-q*la*(0.25*Ca-0.5*h)-P)/(np.cos(theta)*0.5*h)
 def reaction_forces(Izz):
+    # Calculation of R, just a moment equation around the hingeline to solve for R.
     R =  (-q*la*(0.25*Ca-0.5*h)*np.cos(np.deg2rad(theta)))/(h*0.5*np.sqrt(2)*np.sin(np.pi*0.25-np.deg2rad(theta))) - P
+    
+    # Calculation or X2, just sum of forces in the x direction, one force hence X2 equals zero
     X2 = 0
+    
+    # Calculation for Y1,Y2,Y3. This is done by using moment equation around hinge 2, sum of forces in y,
+    # and 3 compatibility equations using the known deflections of hinges 1,2 and 3. 
     y_force = sympy.Matrix([[(1/6)*(x2-x1)**3, 0, 0, x2, 1, (1/24)*q*x2**4],
                               [0, 0, 0, x1, 1, d1*E*Izz + (1/24)*q*x1**4],
                               [(1/6)*(x3-x1)**3, (1/6)*(x3-x2)**3, 0, x3, 1, d3*E*Izz + (1/24)*q*x3**4],
                               [x2-x1, 0, -x3+x2, 0, 0, 0.5*q*x2**2 - 0.5*q*(x3-x2)**2],
                               [1, 1, 1, 0, 0, la*q]])
     
+    # Calculation for Z1,Z2,Z3. This is done by using moment equation around hinge 2, sum of forces in z,
+    # and 3 compatibility equations using the known deflections of hinges 1,2 and 3. 
     z_force = sympy.Matrix([[-1,-1,-1,0,0,R+P],
                             [x1-x2,0,x3-x2,0,0, -0.5*xa*(P-R)],
                             [0,0,0,x1,1,0],
                             [(1/6)*(x2-x1)**3,0,0,x2,1,-(1/6)*(-0.5*xa)**3*R],
                             [(1/6)*(x3-x1)**3, (1/6)*(x3-x2)**3,0,x3,1, -(1/6)*(x3-(x2-0.5*xa))**3*R - (1/6)*(x3-(x2+0.5*xa))**3*P]])
     
-    
+    # Row reducing the two matrices to solve for the forces.
     rrefy = y_force.rref()[0]
     rrefz = z_force.rref()[0]
     
+    # Extracting the results.
     Y1, Y2, Y3, yA, yB = rrefy[5], rrefy[11], rrefy[17], rrefy[23], rrefy[29]
     Z1, Z2, Z3, zA, zB = rrefz[5], rrefz[11], rrefz[17], rrefz[23], rrefz[29]
     
     return float(X2), float(Y1),float(Y2),float(Y3),float(Z1),float(Z2),float(Z3),R
+    # In order to test the results, comment out the return statement.
     
-
-
-
-
-
-
-
+    # Below are the equation used for the calculations.
     def test_reactionforcesy():
         margin = 0.0001
         assert -margin <= Y1 + Y2 + Y3 -la*q <= margin
