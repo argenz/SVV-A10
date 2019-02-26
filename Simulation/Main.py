1d3acba0991b7b87de3e3c5af74d2b2a0d8d616f
Dirk <dirk_de_boer@hotmail.com>
2019-02-26 16:22:15 +0100

    bending update

--- Simulation/Main.py
+++ Simulation/Main.py
@@ -7,6 +7,7 @@ Created on Wed Feb 13 18:42:03 2019
 Main function to calculate delfection and shear flow of A320 aileron
 """
 # Reading data and initializing libraries and other functions.
+import matplotlib.pyplot as plt
 import numpy as np    
 from Modules.reactionforces import *
 from Modules.centroid import *
@@ -33,7 +34,11 @@ shear_center = get_shear_center(Izz,tsk,h,Ca)
 # Obtaining the reaction forces and their transform. forces in internal coordinatesystem!!    
 U2,V1,V2,V3,W1,W2,W3,Q_v,Q_w,R_v,R_w,P_v,P_w,thetaz,thetay = reaction_forces(Iyy,Izz)
 
-
 """ Maybe use this naming of the internal rf?"""
 
 xcoordinatesdiscr,ydefbending,zdefbending,x1discr,x3discr=deformduetobending()
+#plt.plot(xcoordinatesdiscr,zdefbending)
+#plt.show()
+
+
+
