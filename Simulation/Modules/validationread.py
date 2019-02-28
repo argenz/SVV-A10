99afffd8d8f28ee839ff78703bb5abdd4d345b7e
mitchelmitchelmitchel <47745651+mitchelmitchelmitchel@users.noreply.github.com>
2019-02-28 17:33:18 +0100

    push

--- Simulation/Modules/validationread.py
+++ Simulation/Modules/validationread.py
@@ -1,3 +1,29 @@
 #to import all validation data from the .xlsx
 import pandas as pd
-df = pd.read_excel('validationall.xlsx')
+import matplotlib.pyplot as plt
+
+TEDY = pd.read_excel('validation_dy_te_le_hinge.xlsx', sheet_name='TE_DY_ALONGX')
+LEDY = pd.read_excel('validation_dy_te_le_hinge.xlsx', sheet_name='LE_DY_ALONGX')
+HINGEDY = pd.read_excel('validation_dy_te_le_hinge.xlsx', sheet_name='HINGE_DY_ALONGX')
+
+TEDY = TEDY.values/1000
+LEDY = LEDY.values/1000
+HINGEDY = HINGEDY.values/1000
+
+#plotting TE DY
+plt.plot(TEDY[:,0],TEDY[:,3])
+plt.ylabel('dY [m]')
+plt.xlabel('x [m]')
+plt.show()
+
+#plotting LE DY
+plt.plot(LEDY[:,0],LEDY[:,3])
+plt.ylabel('dY [m]')
+plt.xlabel('x [m]')
+plt.show()
+
+#plotting HINGE DY
+plt.plot(HINGEDY[:,0],HINGEDY[:,3])
+plt.ylabel('dY [m]')
+plt.xlabel('x [m]')
+plt.show()
